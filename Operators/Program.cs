abfbb7244e485c5998c493d09d455614210c5b09
none <moeez_agha@outlook.com>
2021-02-25 19:03:44 -0800

    Add project files.

---
+++ Operators/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = 12;
+
+            if (number == 1 )
+            {
+                Console.WriteLine($"The number is equal to {number}");
+            }
+            else
+            {
+                Console.WriteLine($"The number is not equal to 1 the value of number =  {number}");
+            }
+
+            if (number is 1)
+            {
+                Console.WriteLine($"The number is equal to {number} using is Keyword");
+            }
+            else
+            {
+                Console.WriteLine($"The number is not equal to 1 the value of number =  {number} using is Keyword");
+            }
+
+
+            Console.WriteLine(number == 1 ? $"The number is equal to { number } using Conditional Operator"
+                : $"The number is not equal to 1 the value of number =  {number} using Conditional Operator");
+
+
+
+        }
+    }
+}
